0f81ca11fb7ab359aa4040de7033f01b6a829e73
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 16:28:54 +0530

    Added:Calculating the wage till condition

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,16 +2,23 @@
 
 echo "Welcome to employee  wage computation"
 
-ispresent=1
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-noWorkingDays=20
+IS_PRESENT=1
+IS_PART_TIME=1
+IS_FULL_TIME=2
+MAX_WORKING_HRS=100
+EMP_RATE_PER_HR=20
+MAX_WORKING_DAYS=20
+
+# Variables
 totalSalary=0
+totalEmpHrs=0
+totalWorkingDays=0
+salary=0
+
 
 randomtemp=$(( RANDOM%3 ))
 
-if [[ $ispresent -eq $randomtemp ]]
+if [[ $IS_PRESENT -eq $randomtemp ]]
 then
 	echo "employee is present"
 else
@@ -20,10 +27,10 @@ fi
 
 # Calculate daily employee wage
 
-if [[ $ispresent -eq $randomtemp ]]
+if [[ $IS_PRESENT -eq $randomtemp ]]
 then
 	empHrs=8;
-	salary=$(( $empHrs*$empRatePerHr ));
+	salary=$(( $empHrs*$EMP_RATE_PER_HRS ));
 else
 	salary=0;
 fi
@@ -31,20 +38,42 @@ fi
 # To add part time employee
 #Using case statement
 
-for((day=1;day<=$noWorkingDays;day++))
+for((day=1;day<=$MAX_WORKING_DAYS;day++))
 do
 	randomtemp=$(( RANDOM%3 ))
 	case $randomtemp in
-		$isFullTime)
+		$IS_FULL_TIME)
 			empHrs=8;
 			;;
-		$isPartTime)
+		$IS_PART_TIME)
 			empHrs=4;
 			;;
 		$*)
 			empHrs=0;
 			;;
 	esac
-	salary=$(($empHrs*$empRatePerHr))
+	salary=$(($empHrs*$EMP_RATE_PER_HRS))
 	totalSalary=$(($totalSalary+$salary))
 done
+
+# Calculate wages till a condition of total working hours is reached for a month
+
+while [[ $totalWorkingDays -lt $MAX_WORKING_DAYS && $totalEmpHrs -lt $MAX_WORKING_HRS ]]
+do
+	((totalWorkingDays++))
+	randomtemp=$((RANDOM%3))
+  	case $randomtemp in
+		$IS_FULL_TIME)
+			empHrs=8
+         ;;
+      $IS_PART_TIME)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
